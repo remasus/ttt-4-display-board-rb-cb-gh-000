0f2b8f8c1c08a01533490efa43e1c1539a361b5c
EC2 Default User <ec2-user@ip-172-31-45-106.us-west-2.compute.internal>
2018-01-07 15:35:10 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(places)
+    puts " #{places[0]} | #{places[1]} | #{places[2]} "
+    puts "-----------"
+    puts " #{places[3]} | #{places[4]} | #{places[5]} "
+    puts "-----------"
+    puts " #{places[6]} | #{places[7]} | #{places[8]} "
+end
